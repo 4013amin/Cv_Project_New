c6548dff3034deba4c12346b26894a65f155842b
NarjesZamani <narjeszamani2001@gmail.com>
2024-10-31 16:40:03 +0330

    hi

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['.cv/app/static/**/*.html',
+  '.cv/app/static/**/*.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
